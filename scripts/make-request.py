26cd1b136e7ef6b0dcc531c34764ba62be30a606
kumavis <aaron@kumavis.me>
2023-10-27 12:15:22 -1000

    disable debug logging for comment submission

--- scripts/make-request.py
+++ scripts/make-request.py
@@ -5,13 +5,13 @@ try:
 except ImportError:
     from httplib import HTTPConnection # py2
 
-# enable verbose logging
-HTTPConnection.debuglevel = 1
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+# # enable verbose logging (will log API key)
+# HTTPConnection.debuglevel = 1
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
 
 # make request
 response = requests.post(os.environ['_url'], json={'body':os.environ['_body']}, headers={'user-agent': os.environ['_user'], 'Authorization': 'token {}'.format(os.environ['_token'])})
